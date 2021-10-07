f90bcf3025f893dd583b3bfb511ffc5eced865d8
Vaibhav-Kambli <vaibhavk2608@gmail.com>
2021-10-06 23:42:20 -0400

    completed 01 exercise

--- exercises/01-es6-modules/index.js
+++ exercises/01-es6-modules/index.js
@@ -1,21 +1,11 @@
 // Refactor this file so that:
-// All functions are in a separate module called math.js 
+// All functions are in a separate module called math.js
 
-function add(a, b) {
-  return a + b;
-}
+import { add, subtract, multiply } from "./math.js";
 
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-var answer1 = add(21, 21);
+let answer1 = add(21, 21);
 console.log(answer1);
-var answer2 = subtract(44, 2);
+let answer2 = subtract(44, 2);
 console.log(answer2);
-var answer3 = multiply(21, 2);
+let answer3 = multiply(21, 2);
 console.log(answer3);
--- exercises/01-es6-modules/math.js
+++ exercises/01-es6-modules/math.js
@@ -1,1 +1,12 @@
 // define and export your functions here!
+export const add = (a, b) => {
+	return a + b;
+};
+
+export const subtract = (a, b) => {
+	return a - b;
+};
+
+export const multiply = (a, b) => {
+	return a * b;
+};
